85240b6002aa3ee9f2f6610c94dcd132fa566d2e
helmy <mohelmy.eng@gmail.com>
2026-01-04 13:15:51 +0200

    feat: Enable SKIE and add Android publishing to workflow

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.sqldelight)
-//    alias(libs.plugins.skie)
+    alias(libs.plugins.skie)
     `maven-publish`
 }
 
