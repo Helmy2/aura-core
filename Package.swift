47dd957c91b979296373ea4a6d5de2a416eeb276
helmy <mohelmy.eng@gmail.com>
2026-01-04 11:09:02 +0200

    feat: Configure KMMBridge for remote SPM package

--- Package.swift
+++ Package.swift
@@ -1,22 +1,29 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://maven.pkg.github.com/Helmy2/aura-core/com/helmy/aura/shared-kmmbridge/0.0.1/shared-kmmbridge-0.0.1.zip"
+let remoteKotlinChecksum = "e3d70ad118debe609444ac53d45dfd2194537b579ec30b270aa501d713fdb211"
+let packageName = "Shared"
+// END KMMBRIDGE BLOCK
+
 let package = Package(
-    name: "Shared",
+    name: packageName,
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         .library(
-            name: "Shared",
-            targets: ["Shared"]
+            name: packageName,
+            targets: [packageName]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "Shared",
-            url: "",
-            checksum: ""
-        ),
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
     ]
 )
