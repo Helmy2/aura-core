380267929e1936f6b7c745c135f5201d706d4a90
GitHub Action <action@github.com>
2026-01-04 11:54:59 +0000

    Update Package.swift for v0.0.1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "URL_PLACEHOLDER",
-            checksum: "CHECKSUM_PLACEHOLDER"
+            url: "https://github.com/Helmy2/aura-core/releases/download/v0.0.1/Shared.xcframework.zip",
+            checksum: "4bef1a5a12ac1a87b0872a0c709b3dc5055a9535b8948e504c50988e25958835"
         ),
     ]
 )
